f32fb9491802918eb2c9ebf4e43e445bea68362c
hleu0019 <hleu0019@student.monash.edu>
2022-11-04 13:58:37 +1100

    removed particle file and just put into sketch.js

--- fireworks/src/sketch.js
+++ fireworks/src/sketch.js
@@ -1,18 +1,35 @@
-var angle = 0;
+var window_x = 1000;
+var window_y = 600;
+var fireworks;
+function Particle(x,y) {
+    this.pos = createVector(x,y);
+    this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
+
+    this.applyForce = function(force) {
+        acc.add(force);
+    }
+    
+    this.update = function() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+    }
+
+    this.show = function() {
+        this.point(this.pos.x, this.pos.y);
+    }
+}
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(window_x, window_y);
+  stroke(255);
+  strokeWeight(4);
+  fireworks = new Particle(100 ,250);
+  
 }
 
 function draw() {
-  background('black');
-  rectMode(CENTER);
-  fill(0,0,255);
-  rotateX(angle);
-  rotateY(angle);
-
-  rect(0,0,150,150);
-
-  angle += 0.01;
+  background(51);
 
 }
